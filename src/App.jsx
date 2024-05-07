3da4b7fd0990de3af6f4cc0138a7d0b07e03c231
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-07 22:47:44 +0530

    React-Router Routes setup - getting started

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,24 @@
+import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+
+// Let's set up our routers
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <h1>Hello World!</h1>,
+  },
+  {
+    path: "/about",
+    element: (
+      <div>
+        <h1>This is the about page</h1>
+      </div>
+    ),
+  },
+]);
+
 const App = () => {
-  return <h2>mixmaster starter</h2>;
+  return <RouterProvider router={router} />;
 };
+
 export default App;
