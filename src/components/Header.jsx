f7f86ac659e8af5485888787c2cc6e54c855851f
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-08 00:37:22 +0530

    Just created a Styled Component Btn

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,19 @@
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const StyledBtn = styled.button`
+  background-color: orange;
+  color: white;
+  padding: 0.5rem 1rem;
+  border: none;
+`;
 
 export default function Header() {
   return (
     <nav>
       <div className="nav-center">
         <span className="logo">MixMaster</span>
+        <StyledBtn>Button Test</StyledBtn>
         <div className="nav-links">
           <NavLink
             className="nav-link"
