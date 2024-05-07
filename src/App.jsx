80f84ae9556dda2ba0157d4e1b9f53eb8775a781
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-08 00:05:57 +0530

    Step-3: index page set in the routes

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ const router = createBrowserRouter([
     element: <HomeLayout />,
     children: [
       {
-        path: "landing",
+        index: true,
         element: <Landing />,
       },
       {
