8ca94584dd1e4d7a34be69c4715eaa947c9e3c25
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-08 00:50:33 +0530

    Styled Common Navbar with styled components

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,11 @@
 import { NavLink } from "react-router-dom";
-import styled from "styled-components";
-
-const StyledBtn = styled.button`
-  background-color: orange;
-  color: white;
-  padding: 0.5rem 1rem;
-  border: none;
-`;
+import Wrapper from "../assets/wrappers/Navbar";
 
 function Header() {
   return (
     <Wrapper>
-      <div className="Wrapper-center">
+      <div className="nav-center">
         <span className="logo">MixMaster</span>
-        <StyledBtn>Button Test</StyledBtn>
         <div className="nav-links">
           <NavLink
             className="nav-link"
@@ -39,7 +31,4 @@ function Header() {
   );
 }
 
-const Wrapper = styled.nav`
-  background-color: teal;
-`;
 export default Header;
--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from "react-router-dom";
-import Header from "../components/Header";
+import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 export default function HomeLayout() {
   return (
     <div>
-      <Header />
+      <Navbar />
       <Outlet />
       <Footer />
     </div>
