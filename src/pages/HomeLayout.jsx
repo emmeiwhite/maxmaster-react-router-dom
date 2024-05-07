0fa2359da18baefd3efe6ba083bed3651f6d5cbf
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-08 00:02:15 +0530

    Step-1: Setting up common layout for all the pages

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,11 @@
-import { Link } from "react-router-dom";
+import { Outlet } from "react-router-dom";
 
 export default function HomeLayout() {
   return (
     <div>
-      <h1>HomePage</h1>
-      <Link to="/about">About</Link>
+      <Header />
+      <Outlet />
+      <Footer />
     </div>
   );
 }
