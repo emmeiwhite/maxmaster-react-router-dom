ad82a0016228872119257d227b7240ea7ccd3d23
Imran Rafiq Rather <emmeiwhite@gmail.com>
2024-05-09 15:22:58 +0530

    useLoaderData to set the data loading before Component mounting

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,38 @@
-import { createBrowserRouter, Router, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, Router, RouterProvider } from 'react-router-dom'
 
-import {
-  About,
-  HomeLayout,
-  Error,
-  Landing,
-  Cocktail,
-  Newsletter,
-} from "./pages";
+import { About, HomeLayout, Error, Landing, Cocktail, Newsletter } from './pages'
+
+import { loader as landingLoader } from './pages/Landing'
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomeLayout />,
     errorElement: <Error />,
     children: [
       {
         index: true,
         element: <Landing />,
+        loader: landingLoader
       },
       {
-        path: "cocktail",
-        element: <Cocktail />,
+        path: 'cocktail',
+        element: <Cocktail />
       },
       {
-        path: "newsletter",
-        element: <Newsletter />,
+        path: 'newsletter',
+        element: <Newsletter />
       },
       {
-        path: "about",
-        element: <About />,
-      },
-    ],
-  },
-]);
+        path: 'about',
+        element: <About />
+      }
+    ]
+  }
+])
 
 const App = () => {
-  return <RouterProvider router={router} />;
-};
+  return <RouterProvider router={router} />
+}
 
-export default App;
+export default App
--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,3 +1,11 @@
+import { useLoaderData } from 'react-router-dom'
+
+export const loader = async () => {
+  return 'some string'
+}
+
 export default function Landing() {
-  return <div>Landing Page | Index Page</div>;
+  const data = useLoaderData()
+  console.log(data)
+  return <div>Landing Page | Index Page</div>
 }
